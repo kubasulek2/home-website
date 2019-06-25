53d7f0d25e6f35f6c4bffdf9e38ccf7c8853a0f4
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-25 14:16:05 +0200

    sourceMaps and cleanCss synchronized

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,11 @@ gulp.task( 'styles', function () {
 	return gulp
 		.src( './src/sass/**/*.scss' )
 		.pipe(sourcemaps.init())
-		.pipe( $.sass().on( 'error', $.sass.logError ) )
+		.pipe( $.sass({outputStyle: 'compressed'}).on( 'error', $.sass.logError ) )
 		.pipe( $.autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
-		.pipe( $.cleanCss() )
 		.pipe(gulpResolveUrl())
-		.pipe(sourcemaps.write())
+		.pipe(sourcemaps.write(''))
+		.pipe( $.cleanCss() )
 		.pipe( gulp.dest( 'public/css' ) )
 		.pipe( browserSync.reload( {stream: true} ) );
 } );
