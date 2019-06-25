3932aac3cf6753ae575988a0fdeb10240ea4ed7a
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-25 14:46:34 +0200

    started styling intro

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,9 @@ gulp.task( 'styles', function () {
 		.pipe(sourcemaps.init())
 		.pipe( $.sass({outputStyle: 'compressed'}).on( 'error', $.sass.logError ) )
 		.pipe( $.autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
+		.pipe( $.cleanCss() )
 		.pipe(gulpResolveUrl())
 		.pipe(sourcemaps.write(''))
-		.pipe( $.cleanCss() )
 		.pipe( gulp.dest( 'public/css' ) )
 		.pipe( browserSync.reload( {stream: true} ) );
 } );
