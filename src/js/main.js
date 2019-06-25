dd749dd543ccb63879329e0646387ce7c4509fe8
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-25 20:38:43 +0200

    loader done, first version of intro done

--- src/js/main.js
+++ src/js/main.js
@@ -5,27 +5,29 @@ $(() => {
 		loader = $('.loader'),
 		tlIntro = new TimelineMax(),
 		tlLoad = new TimelineMax(),
-		myFunc = () => {
-			console.log('aaa');
+		tlDots = new TimelineMax();
+		
+	const loadingAnimation = () => {
+		tlLoad.staggerFrom(loader, 1, { opacity: 0, y: -100, ease: Bounce.easeOut, repeat: -1, repeatDelay: .2 }, .1);
+	};
+	const glimpseAnimation = () => {
+		tlIntro.to(glimpse, 0.5, { y: '-100%' })
+			.set(glimpse, { y: '100%' });
+	};
 
-		};
+	const introVidReady = () => {
+		
+		const introVid = document.querySelector('.bg-vid video');
+		introVid.readyState === 4 ? quitIntro() : setTimeout(() => introVidReady(), 2000);
+	};
+	const quitIntro = () => {
+		tlLoad.kill();
+		TweenMax.to(loader, 1, { opacity: 0, onComplete: glimpseAnimation });
+	};
+	loadingAnimation();
+	introVidReady();
 
 
-	tlLoad.staggerFrom(loader, 1, { opacity: 0, y: -100, ease: Bounce.easeOut, repeat: -1, repeatDelay: .2 }, .1);
-	$(window).on('load', function () {
-		setTimeout(() => {
-
-
-			tlLoad.kill();
-			TweenMax.to(loader, 1, { opacity: 0, onComplete: myFunc });
-
-		}, 1800);
-
-	});
-
-
-
-
-
-
+	
+	
 });
