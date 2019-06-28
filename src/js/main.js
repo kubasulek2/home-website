6a3fe133506364e57330b0e447aa36fe24f7a65b
Kuba Sukowski <kubasulek2@gmail.com>
2019-06-28 16:49:52 +0200

    image glitch done

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,54 @@ const isMobileDevice = () => {
 const viewPortWidth = () => {
 	return $(window).outerWidth();
 };
+/* update Modernizr to recognize support for CSS clip-path polygon */
+(function(Modernizr){
+
+	// Here are all the values we will test. If you want to use just one or two, comment out the lines of test you don't need.
+	var tests = [
+		{ name: 'polygon', value: 'polygon(50% 0%, 0% 100%, 100% 100%)' }
+	];
+
+	var t = 0,
+			name, value, prop;
+
+	for (; t < tests.length; t++) {
+		name = tests[t].name;
+		value = tests[t].value;
+		Modernizr.addTest('cssclippath' + name, function(){
+			// Try using window.CSS.supports
+			if ( 'CSS' in window && 'supports' in window.CSS ) {
+				for (var i = 0; i < Modernizr._prefixes.length; i++) {
+					prop = Modernizr._prefixes[i] + 'clip-path'
+					
+					if ( window.CSS.supports(prop,value) ) { return true; }
+				}
+				return false;
+			}
+			// Otherwise, use Modernizr.testStyles and examine the property manually
+			return Modernizr.testStyles('#modernizr { '+Modernizr._prefixes.join('clip-path:'+value+'; ')+' }',function(elem, rule) {
+				var style = getComputedStyle(elem),
+						clip = style.clipPath;
+
+				if ( !clip || clip == "none" ) {
+					clip = false;
+
+					for (var i = 0; i < Modernizr._domPrefixes.length; i++) {
+						test = Modernizr._domPrefixes[i] + 'ClipPath';
+						if ( style[test] && style[test] !== "none" ) {
+							clip = true;
+							break;
+						}
+					}
+				}
+
+				return Modernizr.testProp('clipPath') && clip;
+			});
+		});
+
+	}
+
+})(Modernizr);
 
 $(document).ready(function () {
 
@@ -65,6 +113,9 @@ $(document).ready(function () {
 					.to($panel.parent(), 1, {  marginLeft: marginLeft, marginRight: 'auto', ease: Power3.easeInOut })
 					.add(appendImage)
 					.from($image, 1, { opacity: 0, x: '-100%', ease: Power3.easeOut});
+
+				if (!Modernizr.cssclippathpolygon) $('.glitch').hide();
+			
 			} else setTimeout(showRightPanel, 200);
 
 		};
