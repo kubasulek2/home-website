fe50796200bb8e700eb3375158247c9a8d94f58f
Kuba Sukowski <kubasulek2@gmail.com>
2019-07-24 12:03:29 +0200

    minified css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const
 	browserSync = require( 'browser-sync' ),
 	$ = require( 'gulp-load-plugins' )( {lazy: true} ),
 	uglify = require('gulp-uglify-es').default,
+	cssmin = require('gulp-cssmin'),
 	sourcemaps = require('gulp-sourcemaps'),
 	gulpResolveUrl = require('gulp-resolve-url');
 browserify = require('gulp-browserify');
@@ -13,8 +14,8 @@ gulp.task( 'styles', function () {
 		.pipe(sourcemaps.init())
 		.pipe( $.sass({outputStyle: 'compressed'}).on( 'error', $.sass.logError ) )
 		.pipe( $.autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
-		.pipe( $.cleanCss() )
 		.pipe(gulpResolveUrl())
+		.pipe(cssmin() )
 		.pipe(sourcemaps.write(''))
 		.pipe( gulp.dest( 'public/css' ) )
 		.pipe( browserSync.reload( {stream: true} ) );
